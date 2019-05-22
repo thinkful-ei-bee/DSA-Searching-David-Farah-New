018d1b729aa27f7372b016fe86517a9a796185dd
Farah Student <farah.gus@hotmail.com>
2019-05-22 15:46:57 -0400

    search BST problem

--- src/other-exercises/howMany.js
+++ src/other-exercises/howMany.js
@@ -22,11 +22,86 @@ function binarySearch(array, value, start = 0, end = array.length) {
   }
 }
 
+function findBook(library, deweyDec){
+  // assuming deweyDec is unique
+  const nums = library.map(arr => arr[0])
+  return library[binarySearch(nums, deweyDec)]
+}
+
+class BST {
+  constructor (key = null, value = null, parent = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function postOrderBST(arr){
+  if (arr.length === 0) {
+    return null;
+  }
+
+  let lastItem = arr[arr.length -1];
+  let bst = new BST(lastItem);
+  let left = arr.filter(x => x<lastItem);
+  let right = arr.filter(x => x>lastItem);
+
+  bst.left = postOrderBST(left);
+  bst.right = postOrderBST(right);
+
+  return bst;
+}
+
+function preOrderBST(node){
+  console.log(node.key)
+
+  if(node.left) {
+    preOrderBST(node.left);
+  }
+  if(node.right){
+    preOrderBST(node.right);
+  }
+}
+
+function inOrderBST(node) {
+  if(node.left) {
+    inOrderBST(node.left);
+  }
+  
+  console.log(node.key);
+
+  if(node.right){
+    inOrderBST(node.right);
+  }
+  
+}
+
+function postOrderTree(node) {
+  if(node.left) {
+    postOrderTree(node.left);
+  }
+  
+  if(node.right){
+    postOrderTree(node.right);
+  }
+  
+  console.log(node.key);
+}
+
 function main() {
   const list = [3, 5, 6, 8, 11, 12, 14, 15, 17, 18];
-  binarySearch(list, 8);
+  // binarySearch(list, 8);
 
-  binarySearch(list, 16);
+  // binarySearch(list, 16);
+
+  const postOrderList = [ 5, 7, 6, 9, 11, 10, 8];
+  const postOrder = postOrderBST(postOrderList);
+
+  // inOrderBST(postOrder);
+  preOrderBST(postOrder);
+  //postOrderTree(postOrder);
 }
 
 main();
@@ -39,3 +114,6 @@ main();
 // numbers: 12, 17, 14, 15
 
 
+//     14,19,15,27,25,79,90,91,89,35
+
+// 8, 6, 5, 7, 10, 9, 11
